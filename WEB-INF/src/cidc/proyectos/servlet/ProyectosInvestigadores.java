3e3ba2beae2089fce9df564c394fa62cfb4e8efe
cesar solano <cesarsolau@hotmail.com>
2013-07-25 14:32:10 -0500

    documentos
    visualización de documentos scaneados

--- WEB-INF/src/cidc/proyectos/servlet/ProyectosInvestigadores.java
+++ WEB-INF/src/cidc/proyectos/servlet/ProyectosInvestigadores.java
@@ -1,32 +1,20 @@
 package cidc.proyectos.servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.fileupload.DiskFileUpload;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import cidc.general.db.BaseDB;
 import cidc.general.db.CursorDB;
 import cidc.general.login.Usuario;
-import cidc.general.obj.CargarDocumento;
 import cidc.general.servlet.ServletGeneral;
-import cidc.proyectos.db.ProyectosDB;
 import cidc.proyectos.db.ProyectosInvestigadorDB;
 import cidc.proyectos.obj.BalanceGeneral;
-import cidc.proyectos.obj.Proyecto;
 import cidc.proyectos.obj.Parametros;
 import cidc.proyectos.obj.ProyectoGenerico;
+import cidc.proyectosGeneral.db.ProyectosGeneralDB;;
 
 
 public class ProyectosInvestigadores extends ServletGeneral {
@@ -40,6 +28,7 @@ public class ProyectosInvestigadores extends ServletGeneral {
 		HttpSession sesion=req.getSession();
 		usuario=(Usuario)sesion.getAttribute("loginUsuario");
 		ProyectosInvestigadorDB proyectosDB=new ProyectosInvestigadorDB(cursor,usuario.getPerfil());
+		ProyectosGeneralDB proyGeneral=new ProyectosGeneralDB(cursor, usuario.getPerfil());
 		ProyectoGenerico proyecto =null;
 		if(sesion.getAttribute("proyectoInvestigador")!=null)
 			proyecto = (ProyectoGenerico)sesion.getAttribute("proyectoInvestigador");
@@ -48,6 +37,7 @@ public class ProyectosInvestigadores extends ServletGeneral {
 
 		switch(accion){
 			case Parametros.cmdVerProyecto:
+				sesion.setAttribute("proyectoDocumentos", proyGeneral.getListaDocAnexos(Long.parseLong(req.getParameter("id")),Integer.parseInt(req.getParameter("tipo"))));
 				sesion.setAttribute("proyectoInvestigador", proyectosDB.getProyecto(req.getParameter("id"),req.getParameter("tipo")));
 				irA="/grupos/proyectos/VerProyecto.jsp";
 			break;
