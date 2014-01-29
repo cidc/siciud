068b05f845fc4b789f7eabd98636d5ad221ebba7
dannyd2c2 <dannyc_c@hotmail.com>
2014-01-29 17:00:49 -0500

    cambioconvenio
    ala

--- WEB-INF/src/cidc/convenios/obj/Convenio.java
+++ WEB-INF/src/cidc/convenios/obj/Convenio.java
@@ -1,78 +1,180 @@
 package cidc.convenios.obj;
 
-public class Convenio {
+import java.io.Serializable;
 
-	private long idConvenio;
-	private int estado;
-	private int tipo;
-	private int numero;
-	private int ano;
+public class Convenio implements Serializable{
+	
+	private int idconvenio;
 	private String nombreConvenio;
-	private String entidades;
-	private String fecha;
-	private String valor;
-	private String compromisos;
 	private String observaciones;
+	private String fecha;
+	private String tipo;
+	private long codigo;
+	private String fechaInicio;
+	private int numDisp;
 
-	public String getEntidades() {
-		return entidades;
+	private String fechaFinalizacion;
+	private String entidades;
+	private String F_Digita;
+	private int V_DuraAnos;
+	private int V_Duradias;
+	private int V_Durameses;
+	private float VEfectivo;
+	private float VEspecie;
+	private String N_UsuDigita;
+    private String estado;
+    
+    private String nombreproyecto= null;
+    private int estadop;
+    private int tipop;
+    private int facultad;
+    private int proycurri;
+    private int grupo;
+    private String objetivo = null;
+    private String resumen = null;
+    private String observacionesp= null;
+    
+    public String getNombreproyecto() {
+		return nombreproyecto;
 	}
-	public void setEntidades(String entidades) {
-		this.entidades = entidades;
+	public void setNombreproyecto(String nombreproyecto) {
+		this.nombreproyecto = nombreproyecto;
 	}
-	public int getEstado() {
+	public int getEstadop() {
+		return estadop;
+	}
+	public void setEstadop(int estadop) {
+		this.estadop = estadop;
+	}
+	public int getTipop() {
+		return tipop;
+	}
+	public void setTipop(int tipop) {
+		this.tipop = tipop;
+	}
+	public int getFacultad() {
+		return facultad;
+	}
+	public void setFacultad(int facultad) {
+		this.facultad = facultad;
+	}
+	public int getProycurri() {
+		return proycurri;
+	}
+	public void setProycurri(int proycurri) {
+		this.proycurri = proycurri;
+	}
+	public int getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(int grupo) {
+		this.grupo = grupo;
+	}
+	public String getObjetivo() {
+		return objetivo;
+	}
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+	public String getResumen() {
+		return resumen;
+	}
+	public void setResumen(String resumen) {
+		this.resumen = resumen;
+	}
+	public String getObservacionesp() {
+		return observacionesp;
+	}
+	public void setObservacionesp(String observacionesp) {
+		this.observacionesp = observacionesp;
+	}
+	
+    
+    
+    
+    
+    
+    
+	public float getVEfectivo() {
+		return VEfectivo;
+	}
+	public void setVEfectivo(float vEfectivo) {
+		VEfectivo = vEfectivo;
+	}
+	public float getVEspecie() {
+		return VEspecie;
+	}
+	public void setVEspecie(float vEspecie) {
+		VEspecie = vEspecie;
+	}
+	
+	
+
+	
+    
+    public int getIdconvenio() {
+		return idconvenio;
+	}
+	public void setIdconvenio(int idconvenio) {
+		this.idconvenio = idconvenio;
+	}
+    
+    public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+    
+	public String getEstado() {
 		return estado;
 	}
-	public void setEstado(int estado) {
+	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	public String getFecha() {
-		return fecha;
+	public int getV_DuraAnos() {
+		return V_DuraAnos;
 	}
-	public void setFecha(String fecha) {
-		this.fecha = fecha;
+	public void setV_DuraAnos(int v_DuraAnos) {
+		V_DuraAnos = v_DuraAnos;
 	}
-	public long getIdConvenio() {
-		return idConvenio;
+	public int getV_Duradias() {
+		return V_Duradias;
 	}
-	public void setIdConvenio(long idConvenio) {
-		this.idConvenio = idConvenio;
+	public void setV_Duradias(int v_Duradias) {
+		V_Duradias = v_Duradias;
 	}
+	public int getV_Durameses() {
+		return V_Durameses;
+	}
+	public void setV_Durameses(int v_Durameses) {
+		V_Durameses = v_Durameses;
+	}
+	
+	
+	
+	public String getF_Digita() {
+		return F_Digita;
+	}
+	public void setF_Digita(String f_Digita) {
+		F_Digita = f_Digita;
+	}
+
+	
+	public String getN_UsuDigita() {
+		return N_UsuDigita;
+	}
+	public void setN_UsuDigita(String n_UsuDigita) {
+		N_UsuDigita = n_UsuDigita;
+	}
+	
+ 
+	
 	public String getNombreConvenio() {
 		return nombreConvenio;
 	}
 	public void setNombreConvenio(String nombreConvenio) {
 		this.nombreConvenio = nombreConvenio;
-	}
-	public int getNumero() {
-		return numero;
-	}
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	public int getTipo() {
-		return tipo;
-	}
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
-	public String getValor() {
-		return valor;
-	}
-	public void setValor(String valor) {
-		this.valor = valor;
-	}
-	public int getAno() {
-		return ano;
-	}
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
-	public String getCompromisos() {
-		return compromisos;
-	}
-	public void setCompromisos(String compromisos) {
-		this.compromisos = compromisos;
 	}
 	public String getObservaciones() {
 		return observaciones;
@@ -80,5 +182,53 @@ public class Convenio {
 	public void setObservaciones(String observaciones) {
 		this.observaciones = observaciones;
 	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public int getNumDisp() {
+		return numDisp;
+	}
+	public void setNumDisp(int numDisp) {
+		this.numDisp = numDisp;
+	}
+
+	public String getFechaFinalizacion() {
+		return fechaFinalizacion;
+	}
+	public void setFechaFinalizacion(String fechaFinalizacion) {
+		this.fechaFinalizacion = fechaFinalizacion;
+	}
+	public String getEntidades() {
+		return entidades;
+	}
+	public void setEntidades(String entidades) {
+		this.entidades = entidades;
+	}
+	
+
+	
+	
+
+	
+
+
 
 }
+
